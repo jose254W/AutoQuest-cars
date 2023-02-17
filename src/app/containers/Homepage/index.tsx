5be56380fb995b43c195258b05f310c37600ff5c
jose254W <josephwaweru069@gmail.com>
2023-02-17 19:49:26 +0300

    initial commite

---
+++ src/app/containers/Homepage/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Navbar } from "../../components/navbar";
+import { TopSection } from "./topSection";
+import { BookCard } from "../../components/bookCard";
+import { Marginer } from "../../components/marginer";
+
+const PageContainer = styled.div`
+ ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+  `}
+`;
+
+export function Homepage () {
+    return( 
+    <PageContainer>
+        <Navbar />
+        <TopSection />
+        <Marginer direction="vertical" margin="2em" />
+        <BookCard />
+    </PageContainer>
+    )
+}
